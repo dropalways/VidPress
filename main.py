211064e560b871a9616666291c30f5bdf4c1e705
always <120043514+dropalways@users.noreply.github.com>
2024-03-05 16:23:44 +0000

    Remove debug

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def main(file_input, file_output):
     while True:
         option = input('Would you like to trim the video? (y/n): ')
         if option.lower() == 'y' or option.lower() == 'yes':
-            print(file_input, file_output)  # debug
+            # print(file_input, file_output)  # debug
             start = input('Where should the video start? (00:00:00) ')
             end = input('Where should the video end? (00:00:00) ')
             os.system(
@@ -21,7 +21,7 @@ def main(file_input, file_output):
             os.remove(file_output)  # remove afterwards
             break
         elif option.lower() == 'n' or option.lower() == 'no':
-            print(file_input, file_output)  # debug
+            # print(file_input, file_output)  # debug
             os.system(
                 f'call compress.bat "{file_input}" "{os.getcwd()}\output\output.mp4"')
             break
